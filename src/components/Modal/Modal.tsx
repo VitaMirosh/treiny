8411e210f867c7b4d3fe043de98a7b10cb46fcd6
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-08-20 00:03:50 +0300

    feat:create reusable Modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import s from "./Modal.module.css"
+import type {ReactNode} from 'react';
+import {createPortal} from 'react-dom';
+
+type Props = {
+  open: boolean
+  onClose?: () => void
+  modalTitle: string
+  children: ReactNode
+}
+
+export const Modal = ({ onClose, open, modalTitle,children }: Props) => {
+  return (
+    <>
+      {open &&
+        <>
+          {createPortal(
+            <div className={s.overlay}>
+              <div className={s.content}>
+                <h3 className={s.title}>{modalTitle}</h3>
+                <hr/>
+                {children}
+                <button className={s.closeIcon} onClick={onClose}>
+                  x
+                </button>
+              </div>
+            </div>, document.body
+          )}
+        </>}
+    </>
+  )
+}
